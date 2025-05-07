4b40c26c33705bfbef6c04da84090812b28f198f
Leila4u <elena.lorenzo55@gmail.com>
2025-05-07 19:11:57 +0200

    Update general.js
    get by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,10 +24,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get("/author/:author", async (req, res) => {
+    const matchingBooks = Object.values(await books).filter(
+      (book) => book.author.toLowerCase() === req.params.author.toLowerCase()
+    );
+    if (matchingBooks.length > 0) {
+      return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+    } else {
+      return res.status(404).json({ message: "No books by that author." });
+    }
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
